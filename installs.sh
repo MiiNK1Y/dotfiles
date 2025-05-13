379f51d544734950227f8f5a996e08f8025f5d76
MiiNK1 <98641699+MiiNK1Y@users.noreply.github.com>
2025-05-13 21:34:38 +0200

    added more apps

--- installs.sh
+++ installs.sh
@@ -1,53 +1,72 @@
 sudo pacman -S --needed \
-	tmux \
-	neovim \
+  # Sway stuff
 	sway \
 	swaybg \
 	waybar \
-	swaylock \
-	fastfetch \
-	foot \
-	git \
-	openssh \
-	firefox \
-	wofi \
-	brightnessctl \
 	swayidle \
-	otf-font-awesome \			# Font for waybar symbols / icons.
-	unzip \
-	fzf \
+	swaylock \
+	swaync \
+	wofi \
+
+  # Terminal + TUI stuff
+	foot \
+	tmux \
+	neovim \
+	fastfetch \
 	htop \
 	btop \
-	man-db \								# Equals "man-pages".
-	thunar \
-	thunar-archive-plugin \ # Archive tool for Thunar context menu.
-	tumbler \ 							# Thumbnailing for images in Thunar.
-	ffmpegthumbnailer \			# Thumbnailing for videos in Thunar.
-	thunar-volman \					# Auto-mounting storage devices for Thunar.
-	grim \									# Native screenshot tool for wayland.
-	pavucontrol \						# GUI audio controller (made for PulseAudio, works with Pipewire).
-	noto-fonts-cjk \				# For asian symbols platform-wide.
-	noto-fonts-emoji \
-	noto-fonts \
+
+  # Other terminal tools
+	git \
+	man-db \
+	fzf \
+	fd \
+	ripgrep \
+	unzip \
 	ffmpeg \
+	grim \
 	yt-dlp \
 
-	qt6-base \							# WARN: PERHAPS UNNEEDED!
-	qt6-wayland \						# WARN: PERHAPS UNNEEDED!
-	libadwaita \            # WARN: PERHAPS UNNEEDED
-
-	vlc \
-	qt5-wayland \						# VLC is based on qt5.
-	nwg-look \              # Theme setter, like LXAppearance.
-	swaync \								# Notification daemon for sway.
-	ripgrep \               # For Lazyvim
-	fd \                    # For Lazyvim
-  gvfs \                  # For Thunar auto-mounting and other features.
-  xorg-xwayland \         # To make steam and other X-apps work.
+  # Applications
+	firefox \
+  chromium \
   qbittorrent \
+	vlc \
+  gimp \
+  libreoffice-fresh \
+  virt-manager \
+  qemu-system-x86 \
+
+  # File-manager (thunar) stuff
+	thunar \
+	tumbler \
+	ffmpegthumbnailer \
+  webp-pixbuf-loader \
+	thunar-archive-plugin \
+	thunar-volman \
+  gvfs \
+
+  # Other tools
   blueman \
+	openssh \
+	brightnessctl \
+	pavucontrol \
+  xorg-xwayland \
+
+  # Fonts
+	noto-fonts \
+	noto-fonts-cjk \
+	noto-fonts-emoji \
+	otf-font-awesome \
+
+  # Styling stuff
+	nwg-look \
   papirus-icon-theme \
-  webp-pixbuf-loader
+	qt5-wayland \
+	qt6-base \
+	qt6-wayland \
+	libadwaita \
+
 
 # Install after enabeling 'multilib' in '/etc/pacman.conf'
 sudo pacman -S --needed \
